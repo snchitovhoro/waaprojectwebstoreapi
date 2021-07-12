bcd3d16f68b1ad4a1f73d3484f44da0aabd84db1
Simbarashe Nicholas Chitovhoro <simbarashenicholaschitovhoro@Simbarashes-MacBook-Air.local>
2021-07-12 12:39:59 -0500

    Initial commit

---
+++ src/main/java/edu/miu/cs545/waaprojectwebstoreapi/WaaprojectwebstoreapiApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.waaprojectwebstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaprojectwebstoreapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaaprojectwebstoreapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs545/waaprojectwebstoreapi/WaaprojectwebstoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.waaprojectwebstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaprojectwebstoreapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
